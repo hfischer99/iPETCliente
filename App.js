91a6fd94b181121bcf4806ce4c8d4d6fe5627fe4
hfischer99 <bernardfischer99@hotmail.com>
2020-08-09 11:19:07 -0300

    iPETCliente

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TapGestureHandler, RotationGestureHandler } from 'react-native-gesture-handler';
+import { NavigationActions } from 'react-navigation';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import {createDrawerNavigator} from 'react-navigation-drawer'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import CustomDrawerContentComponent from './src/Components/Drawer';
+import Teste from './src/screen/Teste'
+Icon.loadFont();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import SignIn from './src/screen/SignIn'
+import AuthLoadingScreen from './src/screen/SignIn/AuthLoadingScreen'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const MainStack = createDrawerNavigator({
+    SignIn: {
+      screen: SignIn
+    },
+    Teste:{
+      screen: Teste
+    }
   },
-});
+  {
+    initialRouteName: 'Teste',
+    contentComponent: CustomDrawerContentComponent,
+    contentOptions: {
+      activeTintColor: '#836FFF',
+      activeBackgroundColor: '#e6e6e6',
+    }
+  });
+  
+  const StackNavigatorContainer = createAppContainer(MainStack);
+  
+  const AuthStack = createStackNavigator(
+    {
+      SignIn: SignIn,
+      App: StackNavigatorContainer,
+      // SignUp: RegisterUser
+      
+      Teste: Teste
+    },
+    {
+      initialRouteName: 'SignIn',
+      headerMode: 'none',
+      header: null,
+    },
+  );
+  
+  const RootStack = createSwitchNavigator(
+    {
+      AuthLoading: AuthLoadingScreen,
+      Auth: AuthStack,
+      App: StackNavigatorContainer,
+      // Auth: AuthStack
+    },
+    {
+      initialRouteName: 'AuthLoading',
+    },
+  );
+  
+  const RootStackContainer = createAppContainer(RootStack);
+  
+  export default RootStackContainer;
+  
---
+++ src/screen/SignIn/index.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react'
+import { StatusBar, ActivityIndicator, AsyncStorage, Image} from 'react-native'
+import { StackActions, NavigationActions } from 'react-navigation'
+import PropTypes from 'prop-types'
+import api from '../../Services/index'
+import {
+  Container,
+  Title,
+  TextInformation,
+  Error,
+  Form,
+  Input,
+  Button,
+  ButtonText,
+} from './styles'
+
+export default function Welcome (props) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  async function saveUser(user) {
+    await AsyncStorage.setItem('@ListApp:userToken', JSON.stringify(user))
+  }
+
+  async function signIn() {
+    if (username.length === 0) return
+
+    setLoading(true)
+
+    try {
+
+      const credentials = {
+        email: username,
+        password: password
+      }
+
+      const response = await api.post('/v1/account/login', credentials)
+
+      const user = response.data
+      await saveUser(user)
+      const resetAction = StackActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({ routeName: 'App', params: {credentials}})],
+      })
+
+      const setParamsAction = NavigationActions.setParams({
+        params: { credentials },
+        key: 'App',
+      });
+
+      setLoading(false)
+     // props.navigation.dispatch(setParamsAction)
+      props.navigation.dispatch(resetAction)
+    } catch (err) {
+      console.log(err)
+
+      setLoading(false)
+      setErrorMessage('Usuário não existe')
+    }
+  }
+
+  return (
+    
+    <Container>
+
+      
+<Image source={require('../../Assets/pngwing.com.png')}
+       style={{
+        width: 300,
+        height: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+        left:17
+        
+
+      }} />
+      
+      <StatusBar barStyle="light-content" />
+
+      <Title>Bem-vindo</Title>
+   
+
+      <TextInformation>
+        Para continuar, precisamos que você informe seu CPF e Senha.
+      </TextInformation>
+
+      {!!errorMessage && <Error>{errorMessage}</Error>}
+
+      <Form>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite seu CPF"
+          underlineColorAndroid="rgba(0, 0, 0, 0)"
+          value={username}
+          onChangeText={username => setUsername(username)}
+        />
+
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite sua senha"
+          underlineColorAndroid="rgba(0, 0, 0, 0)"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={password => setPassword(password)}
+        />
+
+        <Button onPress={signIn}>
+          {loading ? (
+            <ActivityIndicator size="small" color="#836FFF" />
+          ) : (
+            <ButtonText>Prosseguir</ButtonText>
+          )}
+        </Button>
+      </Form>
+    </Container>
+   )
+  }
+  
+  Welcome.navigationOptions = () => {
+    return {
+      headerShown: false,
+    }
+  }
+  
+  Welcome.propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  }
